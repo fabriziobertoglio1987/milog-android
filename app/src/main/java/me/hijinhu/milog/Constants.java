84c471db525083a783887d2abae265715d409a77
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-05-05 15:39:23 +0200

    testing git with sudo studio

--- app/src/main/java/me/hijinhu/milog/Constants.java
+++ app/src/main/java/me/hijinhu/milog/Constants.java
@@ -4,6 +4,8 @@ package me.hijinhu.milog;
  * Constants: settings constants for app
  *
  * Created by kumho on 17-1-8.
+ *
+ * A test as system user
  */
 public class Constants {
     public static final boolean DEBUG = true;
