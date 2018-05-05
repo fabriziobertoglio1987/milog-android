50ddf91f20d41de86f328a9d4dfd5c8e992de684
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-05-05 13:08:14 +0200

    configuring studio after installing Ubuntu 16.05
    resetting the operating system and installing ubuntu 16.04
    reinstalling android studio

--- app/src/main/java/me/hijinhu/milog/Constants.java
+++ app/src/main/java/me/hijinhu/milog/Constants.java
@@ -8,7 +8,7 @@ package me.hijinhu.milog;
 public class Constants {
     public static final boolean DEBUG = true;
 
-    public static final String HOST_URL = "http://192.168.32.187:3000";
+    public static final String HOST_URL = "http://192.168.0.18:3000";
 
     public static final int REQUEST_SELECT_FILE = 0x100;
 }
