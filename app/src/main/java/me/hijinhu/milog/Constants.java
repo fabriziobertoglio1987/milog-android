c5984922e88b94780bc2390389ee9798a50f5cba
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-01-16 16:22:49 +0100

    commit before video presentation

--- app/src/main/java/me/hijinhu/milog/Constants.java
+++ app/src/main/java/me/hijinhu/milog/Constants.java
@@ -8,7 +8,7 @@ package me.hijinhu.milog;
 public class Constants {
     public static final boolean DEBUG = true;
 
-    public static final String HOST_URL = "http://192.168.0.18:3000";
+    public static final String HOST_URL = "https://languagegame.xyz";
 
     public static final int REQUEST_SELECT_FILE = 0x100;
 }
